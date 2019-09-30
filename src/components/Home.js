5e2f1ff9889ff264e42b4238bc0c7e195042c324
RehanRabbani <rabbanirehan6@gmail.com>
2019-09-30 22:41:32 +0500

    starting poemon

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = ()=>{
+    return(
+    <div className="container">
+     <h4 className="center">Home</h4>
+     <p>jskajksajkdjkjsakdj,dsjkdj,lsjkdjas,jsdjkas</p>
+    </div>
+    )
+}
+export default Home;
